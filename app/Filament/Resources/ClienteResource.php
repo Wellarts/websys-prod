b85efd63935d999d3a61ff53ea2fd6b9bfacaf79
Wellington Cavalcante <wellarts@gmail.com>
2023-02-08 16:04:21 -0300

    Clientes

---
+++ app/Filament/Resources/ClienteResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ClienteResource\Pages;
+use App\Filament\Resources\ClienteResource\RelationManagers;
+use App\Models\Cliente;
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ClienteResource extends Resource
+{
+    protected static ?string $model = Cliente::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nome'),
+                TextInput::make('cnpj_cpf')
+                    ->label('CPF/CNPJ'),
+                Textarea::make('endereco')
+                    ->label('Endereço Completo'),
+                TextInput::make('telefone'),    
+                TextInput::make('estado_id')
+                    ->label('Estado'),
+                TextInput::make('cidade_id')
+                    ->label('Cidade'),
+                TextInput::make('email'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageClientes::route('/'),
+        ];
+    }    
+}
