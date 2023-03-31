0e4c760a732e05648bdb13749ae64ab5037ed5a1
Wellington Cavalcante <wellarts@gmail.com>
2023-03-31 15:17:25 -0300

    provide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,36 +41,9 @@ class AppServiceProvider extends ServiceProvider
             'https://unpkg.com/@alpinejs/mask@3.x.x/dist/cdn.min.js',
         ], true);
         
-       /*      Filament::serving(function () {
-                    Filament::registerNavigationItems([
-                        NavigationItem::make('Lucros por Venda')
-                            ->url(route('filament.resources.vendas.lucro'))
-                            ->icon('heroicon-o-presentation-chart-line')
-                            ->activeIcon('heroicon-s-presentation-chart-line')
-                            ->group('Consultas')
-                            ->sort(3),
-                    ]);
-                });
-/*
-        Filament::serving(function () {
+       
 
-            $Expira = Config::find(1);
-            $validade = $Expira->validade;
-            $hoje = date('Y-m-d');
-
-            if($validade <= $hoje)
-                {
-                    Notification::make()
-                    ->title('LICENÇA EXPIRADA!')
-                    ->danger()
-                    //->persistent()
-                    ->send();
-
-                    Auth::logout();
-                    return to_route('filament.auth.login')->with('Unauthorized');
-                }
-
-        });
+       
 
    }
 
